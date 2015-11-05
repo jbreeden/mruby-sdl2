86c9dfdee99ec0891940c9d0228b86e9aa44351e
jbreeden <jared.breeden@gmail.com>
2015-11-05 07:57:16 -0800

    Use SDL_Delay rather than APR.apr_sleep

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -1,3 +1,3 @@
 SDL::SDL_Init(SDL::SDL_INIT_VIDEO)
-window = SDL::SDL_CreateWindow("Titties!", 100, 100, 640, 480, SDL::SDL_WINDOW_SHOWN)
-APR.apr_sleep(5000000)
+window = SDL::SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL::SDL_WINDOW_SHOWN)
+SDL.SDL_Delay(5000)
